5d4c39b5180be1d17850cde994b59aa46202302d
zaq5278 <527895805@qq.com>
2016-05-20 10:42:26 +0800

    sendCar-angular

---
+++ js/app.js
@@ -0,0 +1,15 @@
+
+angular.module('myApp', ['ionic','myApp.sendCarController'])
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+});
+var myUrl = 'http://222.88.22.70:9999/';
